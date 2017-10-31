e29dd9ea88ed0864516fcf7037a113e8e2223e4c
Stephanie Savoie <stephsavoie4@gmail.com>
2017-10-31 14:20:11 -0400

    completed edit and index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Movie.quote,
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quos, nihil. Deleniti nesciunt laboriosam expedita aliquam molestiae placeat nemo vel quo delectus, asperiores voluptatibus eaque, unde error, ratione praesentium temporibus."
+  )
+  article.save!
+end
+puts 'Finished!'
